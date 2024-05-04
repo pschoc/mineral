cd3f02715b25128ed2f021166e01a8c476e2d44c
etaoxing <etaoxing@users.noreply.github.com>
2024-05-03 23:38:56 -0400

    Rename env to cartpoleswingup for dflex

--- mineral/envs/dflex.py
+++ mineral/envs/dflex.py
@@ -5,7 +5,7 @@ from omegaconf import OmegaConf
 
 DEFAULT_DFLEXENVS_KWARGS = {
     'ant': dict(env_name='AntEnv', episode_length=1000, MM_caching_frequency=16),
-    'cartpole': dict(env_name='CartPoleSwingUpEnv', episode_length=240, MM_caching_frequency=4),
+    'cartpoleswingup': dict(env_name='CartPoleSwingUpEnv', episode_length=240, MM_caching_frequency=4),
     'cheetah': dict(env_name='CheetahEnv', episode_length=1000, MM_caching_frequency=16),
     'hopper': dict(env_name='HopperEnv', episode_length=1000, MM_caching_frequency=16),
     'humanoid': dict(env_name='HumanoidEnv', episode_length=1000, MM_caching_frequency=48),
